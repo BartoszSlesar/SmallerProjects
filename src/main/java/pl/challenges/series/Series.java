4e9bbfcf5908dc2d3ebfbe5afa31e1a72eec5883
Bartosz Slesar <bartoszslesar@gmail.com>
2021-05-21 19:17:05 +0200

    Added factorial method, added test cases for factorial method

--- src/main/java/pl/challenges/series/Series.java
+++ src/main/java/pl/challenges/series/Series.java
@@ -10,4 +10,14 @@ public class Series {
         }
         return result;
     }
+
+    public static long factorials(long n) {
+        if (n < 0  || n > 20) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorials(n - 1);
+    }
 }
--- src/test/java/pl/challenges/series/SeriesTest.java
+++ src/test/java/pl/challenges/series/SeriesTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 import java.util.stream.Stream;
@@ -13,14 +14,13 @@ import java.util.stream.Stream;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test Cases for Series")
 public class SeriesTest {
 
     @ParameterizedTest
-    @MethodSource("provideNumbersAndExpectedResult")
+    @MethodSource("provideNumbersAndExpectedResultNSum")
     public void nSumShouldReturnSumOfNNumbers(long n, long expected) {
         //given
         //when
@@ -38,7 +38,39 @@ public class SeriesTest {
         assertTimeout(Duration.ofMillis(5), () -> Series.nSum(1000000));
     }
 
-    private static Stream<Arguments> provideNumbersAndExpectedResult() {
+    @ParameterizedTest
+    @MethodSource("provideNumbersAndExpectedResultfactorial")
+    public void factorialShouldReturnCorrectResult(long n, long expected) {
+        //given
+        //when
+        long result = Series.factorials(n);
+        //then
+        assertThat(result, equalTo(expected));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {100, 21, -433, 11223})
+    public void factorialShouldReturnZeroForValuesLessThenZeroAndGreaterThen20(long n) {
+        //given
+        //when
+        long result = Series.factorials(n);
+        //then
+        assertThat(result, equalTo(0L));
+    }
+
+
+    private static Stream<Arguments> provideNumbersAndExpectedResultfactorial() {
+        return Stream.of(
+                Arguments.of(5, 120),
+                Arguments.of(10, 3628800),
+                Arguments.of(15, 1307674368000L),
+                Arguments.of(0, 1),
+                Arguments.of(20, 2432902008176640000L),
+                Arguments.of(11, 39916800)
+        );
+    }
+
+    private static Stream<Arguments> provideNumbersAndExpectedResultNSum() {
         return Stream.of(
                 Arguments.of(10, 55),
                 Arguments.of(30, 465),
